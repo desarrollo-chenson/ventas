8a5375aba57675d173635bb67febea4360fcc617
Lizbeth Andrea Corrales Prott <andrea.prott@chenson.com.mx>
2020-01-30 12:35:00 -0600

    Actualización footer

--- xenzon/template_xenzon/footer.php
+++ xenzon/template_xenzon/footer.php
@@ -1,19 +1,19 @@
 <footer>
-      <div class="container-fluid footer py-4" >
-        
-
-            <div class="container d-flex justify-content-around pb-5" id="politicas">
-              <div class="row d-flex justify-content-center text-center ">
-                <div class="col-md-3 col-12 d-flex justify-content-center py-2 py-md-0">
-                        <a href="http://hbhandbags.com.mx/politicas-de-privacidad/">POLITICAS DE PRIVACIDAD</a>
+        <div class="container-fluid footer py-4" >
+                <div class="container d-flex justify-content-around pb-5" id="politicas">
+                        <div class="row d-flex justify-content-center text-center ">
+                                <!--
+                                <div class="col-md-3 col-12 d-flex justify-content-center py-2 py-md-0">
+                                        <a href="http://hbhandbags.com.mx/politicas-de-privacidad/">POLITICAS DE PRIVACIDAD</a>
+                                </div>
+                                <div class="col-md-3 col-12 d-flex justify-content-center py-2 py-md-0">
+                                        <a href="http://hbhandbags.com.mx/terminos-de-condiciones/">TÉRMINOS DE CONDICIONES DE USO</a>
+                                </div>
+                                -->
+                                <div class="col-md-6 col-12 d-flex justify-content-center py-2 py-md-0">
+                                        <a href="https://chenson.com.mx/">Todos los derechos reservados © CHENSON NYLON DE MÉXICO SA DE CV 2020.</a>
+                                </div>
+                        </div>
                 </div>
-                <div class="col-md-3 col-12 d-flex justify-content-center py-2 py-md-0">
-                        <a href="http://hbhandbags.com.mx/terminos-de-condiciones/">TÉRMINOS DE CONDICIONES DE USO</a>
-                </div>
-                <div class="col-md-6 col-12 d-flex justify-content-center py-2 py-md-0">
-                        <a href="#">Chenson Nylon de México S.A. de C.V. <?php echo date("Y"); ?> TODOS LOS DERECHOS RESERVADOS</a>
-                </div>
-              </div>
-            </div>
         </div>
 </footer>
